ffb40fbd2938d9d644e5db8742677318692f49da
Hatem <hatem.deux@gmail.com>
2020-02-16 19:25:39 -0500

    fetcher.js (file downloader) and edge cases handled

--- fetcher.js
+++ fetcher.js
@@ -8,9 +8,9 @@ const localFile = process.argv[3]
 ///////////////////////////////////////////////
 
 request(website, (error, response, body) => {
-  // console.log('error:', error); // Print the error if one occurred
+  console.log('error:', error); // Print the error if one occurred
   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
+  // console.log('body:', body); // Print the HTML for the homepage.
   fs.writeFile(localFile, body, err => {
     if (err) throw err;
     if (!response) {
